5ee76934328c1a3ed2bb04040eccdae8c35ac476
Gokula Krishnan S <32614778+iam-gokul@users.noreply.github.com>
2019-10-13 08:19:35 +0530

    Updated Outgoing calls for >8.0

--- app/src/main/java/com/notes/eversimple/PhoneStateReceiver.java
+++ app/src/main/java/com/notes/eversimple/PhoneStateReceiver.java
@@ -1,31 +1,41 @@
-package com.notes.eversimple;
+package com.notes.phonememo;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
-import android.preference.PreferenceManager;
-import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.widget.Toast;
 
 
-
+import static android.content.Context.TELEPHONY_SERVICE;
 import static android.content.Intent.EXTRA_PHONE_NUMBER;
-import static com.notes.eversimple.FloatingViewService.callPhoneNumber;
+import static com.notes.phonememo.FloatingViewService.callPhoneNumber;
 
 public class PhoneStateReceiver extends BroadcastReceiver {
 
 
-    private static final String TAG = "Everee";
+
+    private static final String TAG = "Sonalll";
     @Override
     public void onReceive( Context arg0,  Intent intent) {
-        Log.i(TAG, "OnReceive");
+        String state=intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+        if(state==null)
+        {
+            //Outgoing call
+            callPhoneNumber=intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+            Log.i("tag","Outgoing number : "+callPhoneNumber);
+        }
+        else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING))
+        {
+            //Incoming call
+            callPhoneNumber= intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            Log.i("tag","Incoming number : "+callPhoneNumber);
+        }
+        Log.d(TAG, "OnReceive");
         String action = intent.getAction();
-        Log.i(TAG, "action : " + action);
+        Log.d(TAG, "action : " + action);
         callHandler(arg0,intent);
 
     }
@@ -34,63 +44,44 @@ public class PhoneStateReceiver extends BroadcastReceiver {
 
 
         try {
-            boolean mmicode=false;
 
-
-            if(arg1.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
-
-                callPhoneNumber = arg1.getStringExtra(EXTRA_PHONE_NUMBER);
-                Log.d("Evereee","Outgoing number "+callPhoneNumber);
-                if(callPhoneNumber.charAt(0)!=('*')){
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-                    Intent offIntent = new Intent(arg0,FloatingViewService.class);
-                    Log.d("Everee","Inside New Outgoing"+callPhoneNumber);
-                    arg0.startForegroundService(offIntent);
-                }
-                else {
-                    Intent offIntent = new Intent(arg0,FloatingViewService.class);
-                    arg0.startService(offIntent);
-                }}
-            }
 
             if(arg1.getAction().equals("android.intent.action.PHONE_STATE")){
 
                 String state = arg1.getStringExtra(TelephonyManager.EXTRA_STATE);
+                Log.d("sonalll ","state is : "+state);
 
-                if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
-                    Log.d(TAG, TAG+"Inside Extra state off hook");
-                    String num=arg1.getStringExtra(EXTRA_PHONE_NUMBER);
 
-                    if(callPhoneNumber.charAt(0)!=('*')){
+                if(state.equals("OFFHOOK")) {
+//                    callPhoneNumber = arg1.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+                    Log.d("sonalll","Inside New Outgoing "+callPhoneNumber);
 
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-                        ;
                         Intent offIntent = new Intent(arg0,FloatingViewService.class);
-
                         arg0.startForegroundService(offIntent);
                     }
                     else {
                         Intent offIntent = new Intent(arg0,FloatingViewService.class);
                         arg0.startService(offIntent);
-                    }}
+                    }
                 }
 
                 else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
                     Log.d(TAG, TAG+"Inside EXTRA_STATE_RINGING");
-                    callPhoneNumber = arg1.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+//                    callPhoneNumber = arg1.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
                     Log.d(TAG, TAG+"incoming number : " + callPhoneNumber);
                     if(callPhoneNumber.charAt(0)!=('*')){
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
                         Intent offIntent = new Intent(arg0,FloatingViewService.class);
-
+                        Log.d("Sonal","Reached 86");
                         arg0.startForegroundService(offIntent);
                     }
                     else {
                         Intent offIntent = new Intent(arg0,FloatingViewService.class);
+                        Log.d("Sonal","Reached 91");
                         arg0.startService(offIntent);
                     }
 
